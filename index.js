5b72c89f759d4f564160e0face7004884688ac3f
sparsha722 <47775831+sparsha722@users.noreply.github.com>
2019-02-27 09:28:15 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-'use strict';
- 
-const {WebhookClient} = require('dialogflow-fulfillment');
-const functions = require('firebase-functions');
-const {dialogflow} = require('actions-on-google');
+//const {WebhookClient} = require('dialogflow-fulfillment');
+//const functions = require('firebase-functions');
+//const {dialogflow} = require('actions-on-google');
+var moment = require('moment');
 
 const WELCOME_INTENT = 'Default Welcome Intent';
 const FALLBACK_INTENT = 'Default Fallback Intent';
@@ -21,17 +20,21 @@ app.intent('FALLBACK_INTENT', (convo) => {
 app.intent('MENU_INTENT', (convo) => {
 var date= new Date();
   var current_time=date.getHours();
-  //var st1=date('8:00 am', "HH:mm a");
-  //var et1=date('10:00 am', "HH:mm a")
-  if(current_time.isBetween (8,10))
+  var st1=moment('8:00 am', "HH:mm a");
+  var et1=moment('10:00 am', "HH:mm a");
+  var st2=moment('12:30 pm', "HH:mm a");
+  var et2=moment('3:00 pm', "HH:mm a");
+  var st3=moment('5:00 pm', "HH:mm a");
+  var et3=moment('7:00 pm', "HH:mm a");
+  if(current_time.isBetween (st1,et1))
   {
     convo.ask("Breakfast in available now. Check out our breakfast menu!");
   }
-  else if(current_time.isBetween (12,15))
+  else if(current_time.isBetween (st2,et2))
   {
     convo.ask("Lunch is avilable now.Check out our Lunch menu!");
   }
-  else if(current_time.isBetween (17,19))
+  else if(current_time.isBetween (st3,et3))
   {
     convo.ask("Snacks are available now.Check out our Snacks menu!");
   }
@@ -42,7 +45,9 @@ var date= new Date();
   
 });
 
-exports.dialogflowFirebaseFulfillment = functions.https.onRequest (app);
+
+    
+  exports.dialogflowFirebaseFulfillment = functions.https.onRequest (app);
     
            
            
